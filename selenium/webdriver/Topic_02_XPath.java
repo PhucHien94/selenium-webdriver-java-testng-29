7361704460c5ce04cf4f4ec58893980a8351a831
HienNguyen <phuchien94@gmail.com>
2023-08-02 23:06:37 +0700

    The first comment of Mei

--- selenium/webdriver/Topic_02_XPath.java
+++ selenium/webdriver/Topic_02_XPath.java
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-//test commit by Mei 12
+//Re-test the first comment of Mei
 public class Topic_02_XPath {
     WebDriver driver;
     String projectPath = System.getProperty("user.dir");
