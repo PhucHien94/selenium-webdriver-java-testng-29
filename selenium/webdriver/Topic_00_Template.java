4d6cca3e6428cc646c3a2e383f792f5bcbd1f6bd
PhucHien <phuchien94@gmail.com>
2023-11-17 10:46:56 +0700

    # Topic 02 Locator

--- selenium/webdriver/Topic_00_Template.java
+++ selenium/webdriver/Topic_00_Template.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -34,5 +35,10 @@ public class Topic_00_Template {
     @Test
     public void TC_02_() {
        
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
     }
 }
